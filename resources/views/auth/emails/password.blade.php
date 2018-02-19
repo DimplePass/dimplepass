a6191f9647f6f2196d3d1bf170fabb5804d92224
bjhansen <bj@alltrips.com>
2018-02-19 14:18:47 -0700

    Update password reset form

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,3 @@
-Click here to reset your password: <a href="{{ $link = url(config('app.url').route('password.reset', $token, false)) }}"> {{ $link }} </a>
+<h2>Dimple Pass</h2>
+
+<h4>Click here to reset your password: <a href="{{ $link = url(config('app.url').route('password.reset', $token, false)) }}"> {{ $link }} </a></h4>
