6cf1dd02dc55c09c4091ed7f9ffab8612e62a9f4
bjhansen <bj@alltrips.com>
2018-02-27 14:40:15 -0700

    Create purchase tables

---
+++ database/migrations/2018_02_27_210801_create_purchases_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('confirmation',20)->nullable();
+            $table->dateTime('purchase_date')->nullable();
+            $table->integer('credit_card_id')->unsigned();
+            $table->string('stripe_id')->nullable();
+            $table->string('stripe_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
