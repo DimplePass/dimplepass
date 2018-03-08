da1a8fd86a7ac5ffe73f0879828505ca450e2ff3
bjhansen <bj@alltrips.com>
2018-03-08 14:05:14 -0700

    Adds content to Only the Best page

--- resources/views/thebest.blade.php
+++ resources/views/thebest.blade.php
@@ -35,16 +35,21 @@
 <div class="container padding-bottom-2x mb-2">
   <div class="row padding-bottom-2x">
     <div class="col-md-5">
-      <img class="d-block w-270 mx-auto rounded mb-3" src="/img/holder-540x540.jpg" alt="">
+      <img class="d-block w-270 mx-auto rounded mb-3 hidden-sm-down" src="/img/features/zion-path-540x540.jpg" alt="Hiking in Zion National Park">
     </div>
     <div class="col-md-7 text-md-left text-center">
       <div class="mt-30 hidden-md-up"></div>
-      <h2>A little heading here</h2>
-      <h3>Bacon ipsum dolor amet hamburger pork loin kevin t-bone sirloin jerky cupim turkey short ribs brisket flank pastrami doner.</h3>
+      <h2>Your personal must-do list.</h2>
+      <h3>Stop sifting through reviews and start booking your trip!</h3>
       <div class="mb-4 mt-4"><hr></div>
-      <h3>Some more important stuff.</h3>
+      <h3>Can't Miss Attractions & Activities</h3>
       <i class="pe-7s-diamond float-md-left gray-light mr-4" style="font-size: 6rem;"></i>
-      <p>Chuck pork belly tri-tip turducken meatloaf, pig short ribs capicola jerky t-bone cow. Pork chop chuck jerky landjaeger venison cupim alcatra turkey ribeye tail ham hock buffalo tenderloin tongue. Cow filet mignon chicken, tri-tip swine meatloaf capicola pork loin kielbasa pork belly hamburger jowl fatback salami. Leberkas shank jowl, venison landjaeger jerky tri-tip.</p>
+      <p>We're picky. We like to hand-select the vendors in each region using one simple rule. Is this the trip that we'd want to take if we had a few days to explore during our well deserved vacation? We strive to work with the best in class operators, resorts, restaurants and activity providers in each area. We place a heavy weight on those that encompass the culture and character of each unique destination. </p>
+      <hr>
+      <h3>Curated by Dimple Pass Members</h3>
+      <i class="pe-7s-chat float-md-left gray-light mr-4" style="font-size: 6rem;"></i>
+      <p>As the Dimple Membership grows, we acquire feedback from real travelers each and every day.  We ask that you get involved as well to let us know if you think we are missing something that should be included in the Pass.  We also ask that you let us know if we've included something that you didn't enjoy.</p>
+
     </div>
   </div>
 </div>
