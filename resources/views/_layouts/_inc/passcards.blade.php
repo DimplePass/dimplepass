1e240c593aba707fe377c23be5668cf99bc9aa74
bjhansen <bj@alltrips.com>
2018-03-22 14:59:43 -0600

    Update to destination images from pass images

--- resources/views/_layouts/_inc/passcards.blade.php
+++ resources/views/_layouts/_inc/passcards.blade.php
@@ -4,8 +4,8 @@
     <div class="col-md-4 col-sm-6">
       <div class="card mb-30"><a class="card-img-tiles" href="{{ route('passes.show', $p->slug) }}">
           <div class="inner">
-            <div class="main-img"><img src="/img/destinations/{{ $p->slug }}-315x278.jpg" alt="{{ $p->name }}"></div>
-            <div class="thumblist"><img src="/img/destinations/{{ $p->slug }}-1-155x137.jpg" alt="{{ $p->name }}"><img src="/img/destinations/{{ $p->slug }}-2-155x137.jpg" alt="{{ $p->name }}"></div>
+            <div class="main-img"><img src="/img/destinations/{{ $p->destinations->first()->slug }}-315x278.jpg" alt="{{ $p->name }}"></div>
+            <div class="thumblist"><img src="/img/destinations/{{ $p->destinations->first()->slug }}-1-155x137.jpg" alt="{{ $p->name }}"><img src="/img/destinations/{{ $p->destinations->first()->slug }}-2-155x137.jpg" alt="{{ $p->name }}"></div>
           </div></a>
         <div class="card-body text-center">
           <h4 class="card-title">{{ $p->name }}</h4>
