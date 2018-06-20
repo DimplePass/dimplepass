83bcb1cf14768b91994a002ed23ef58fff073482
bjhansen <bj@alltrips.com>
2018-06-20 14:13:58 -0600

    Add Sticky Get Your Pass footer

--- resources/views/passes/show.blade.php
+++ resources/views/passes/show.blade.php
@@ -26,13 +26,12 @@
         <div class="hero-text">
           <h2 class="mt-0 mb-2 white-color"><strong>{{ $pass->name }} Pass</strong></h2>  
           @if (count($pass->discounts))
-            <h3 class="text-warning"><strong>Unlock discounts on {{ $pass->destinations->first()->short_name }}'s Top Activities.</strong></h3>
+            <h3 class="white-color">Your <strong class="text-warning">${{ number_format($pass->price/100, 0, '.', ',') }} pass</strong> unlocks <strong class="text-warning">{{ count($pass->discounts) }} discounts</strong>{{--  for up to <strong class="text-warning">5 people</strong> --}}.</h3>
           @else
             <h3 class="text-warning"><strong>Available <span class="dp-warning">June 15th.</span></strong></h3>
           @endif
         </div>
       </div>
-
     </div>
   </div>
 </section>
@@ -44,22 +43,6 @@
 <div class="container padding-bottom-3x mb-1 mt-5">
   <div class="row">
     <div class="col-xl-9 col-lg-9 col-md-9 order-md-2">
-
-      {{-- Get Outside Pass CTA Bar --}}
-      <div class="shop-toolbar padding-bottom-1x mb-2">
-        <div class="column">
-          <h3 class="mb-1"><strong>All profits go to programs that Get Kids Outdoors!</strong></h3>
-          <h3 class="mb-1">Your <strong class="text-warning">${{ number_format($pass->price/100, 0, '.', ',') }} pass</strong> unlocks <strong class="text-warning">{{ count($pass->discounts) }} discounts</strong>{{--  for up to <strong class="text-warning">5 people</strong> --}}.</h3>
-          <h6 class="mb-0">Valid {{ $pass->start->format('F jS, Y') }} - {{ $pass->end->format('F jS, Y') }} <small>Dates vary per vendor.</small></h6>
-        </div>
-        <div class="column">
-            <h2 class="mb-1"><strong></strong><a href="{{ route('checkout.payment', ['pass_id' => $pass->id]) }}" class="btn btn-primary btn-xl btn-block" onClick="ga('send', 'event', 'BuyPass-TopRight', '{{ Request::path() }}', '{{ $pass->id }}');">Get the Pass</a></h2>
-            <h6 class="mt-1 text-center">Good for up to 5 people</h6>
-        </div>
-      </div>
-
-     
-
 
       {{-- Vendor Listing --}}
       @foreach ($pass->discounts->where('active', '=', 1)->shuffle() as $d)
@@ -118,9 +101,9 @@
           {{-- Filters --}}
           @include('/passes/_inc/filters')
         </aside>
-  			<hr class="mb-5 hidden-sm-down">
-  			<aside class="text-center">
-            <h2><strong></strong><a href="{{ route('checkout.payment', ['pass_id' => $pass->id]) }}" class="btn btn-primary btn-xl btn-block" onClick="ga('send', 'event', 'BuyPass-LeftSticky', '{{ Request::path() }}', '{{ $pass->id }}');">Get the Pass</a></h2>
+  			<hr class="mb-5 hidden-lg-down">
+  			<aside class="text-center hidden-lg-down">
+            <h2><strong></strong><a href="{{ route('checkout.payment', ['pass_id' => $pass->id]) }}" class="btn btn-primary btn-xl btn-block" onClick="ga('send', 'event', 'BuyPass-LeftSticky', '{{ Request::path() }}', '{{ $pass->id }}');">Get your <strong>${{ number_format($pass->price/100, 0, '.', ',') }}</strong> Pass</a></h2>
             <h6 class="mt-1 text-center">Good for up to 5 people</h6>
   			</aside> 
       </div>
@@ -155,17 +138,11 @@
   </div>
 </div>
 
-
-
-<div class="footerDrawer">
-  <div class="open">
-    <h3 class="white-color"><small><i class="fa fa-chevron-down white-color"></i></small> <strong>Buy Pass Now for $18!</strong></h3>
-  </div>
-  <div class="content">
-    <h4><strong>Help us celebrate National Get Outdoors Day.</strong></h4>
-    <h5><strong>Save 50%</strong> on pass purchase until June 9th!</h5>
-    <a href="{{ route('checkout.payment', ['pass_id' => $pass->id, 'discount' => '1800']) }}" class="btn btn-primary btn-rounded btn-lg mt-3">Buy for $18</a>
-  </div>
+<div class="stickyFooter hidden-xl-up">
+    <a href="{{ route('checkout.payment', ['pass_id' => $pass->id]) }}" onClick="ga('send', 'event', 'BuyPass-StickyFooter', '{{ Request::path() }}', '{{ $pass->id }}');">
+      <h3 class="white-color"><strong>Get your <strong>${{ number_format($pass->price/100, 0, '.', ',') }}</strong> Pass <i class="fa fa-arrow-right"></i></strong></h3>
+      <h6 class="mt-1 text-center dp-info">Good for up to 5 people.</h6>
+    </a>
 </div>
 
 @endif
@@ -174,34 +151,6 @@
 
 @section('scripts')
 <script>
-
-//////////
-/// On Page Load
-//////////
-
-$(function() {
-
-// If Yellowstone Pass
-@if ($pass->id == 1)
-
-  // Open/Close on click.
-  $('.footerDrawer .open').on('click', function() {
-    $('.footerDrawer .content').slideToggle();
-    setTimeout(function(){
-      $('#launcher').fadeToggle();
-    }, 300);
-  });
-  
-  // Open after 20 seconds.
-  // setTimeout(function(){
-  //   $('.footerDrawer .open').show();
-  //   $('.footerDrawer .content').slideToggle();
-  //   $('#launcher').hide();
-  // }, 20000);
-
-@endif
-
-});
 
 // Checkbox towns filter.
 $("#filters :checkbox").click(function() {
