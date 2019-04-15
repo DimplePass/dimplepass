46429e62163cf5c36e28105680a5a6a059f8dd5c
bjhansen <bj@alltrips.com>
2019-04-15 09:03:39 -0600

    remove countdown timer

--- resources/views/passes/show.blade.php
+++ resources/views/passes/show.blade.php
@@ -48,13 +48,13 @@
 {{-- Show Pass if actively selling. --}}
 @if (count($pass->discounts))
 
-<section id="promotion" class="stickyPromo">
+{{-- <section id="promotion" class="stickyPromo">
   <div class="row">
     <div class="col-sm-12 text-center mt-2">
       <h5 class="dp-white"><small>7 Day Special - 50% Off - </small><strong>$14</strong> <strike>$28</strike><br class="hidden-sm-up"><span class="hidden-xs-down"> | </span><strong id="countdown"></strong> <small>until it ends</small></h5>
     </div>
   </div>
-</section>
+</section> --}}
 
 {{-- Page Content --}}
 <div class="container padding-bottom-3x mb-1 mt-5">
@@ -549,35 +549,35 @@ function roundTo(num,places) {
     return calc.toFixed(0);
 }
 
-// Countdown Timer
-// Set the date we're counting down to
-var countDownDate = new Date("Apr 13, 2019 00:00:00").getTime();
+// // Countdown Timer
+// // Set the date we're counting down to
+// var countDownDate = new Date("Apr 13, 2019 00:00:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// // Update the count down every 1 second
+// var x = setInterval(function() {
 
-  // Get todays date and time
-  var now = new Date().getTime();
+//   // Get todays date and time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  // document.getElementById("countdown").innerHTML = days + "days " + hours + "h " + minutes + "m " + seconds + "s ";
-  document.getElementById("countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+//   // Display the result in the element with id="demo"
+//   // document.getElementById("countdown").innerHTML = days + "days " + hours + "h " + minutes + "m " + seconds + "s ";
+//   document.getElementById("countdown").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+//   // If the count down is finished, write some text 
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("demo").innerHTML = "EXPIRED";
+//   }
+// }, 1000);
 
 //////////
 /// Destination Map with Offers
